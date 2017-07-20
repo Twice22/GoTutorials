79b42dfa1da20313767621edc2ca6809d480ec6c
Twice22 <victor.busa@gmail.com>
2017-07-20 17:12:40 +0200

    Add examples Chapter 8.3

---
+++ src/Chapter8/reverb1/reverb.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+// go build
+// ./reverb1.exe &
+// in a cmd command prompt (not on github for windows prompt!!!)
+// netcat2.exe
+// your_text
+func main() {
+	l, err := net.Listen("tcp", "localhost:8001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
